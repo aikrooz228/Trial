29b4a48af3a360c770c89a1b4ad055d12d76edf7
aikrooz228 <aikrooz228@gmail.com>
2024-12-21 18:47:11 -0800

    language

---
+++ cos project.py
@@ -0,0 +1,25 @@
+
+
+
+
+language={'Gida': 'House',
+          'Ruwa': 'Water',
+          'Abinci': 'Food',
+          'Mutum': 'Person',
+          'Yaro': 'Boy',
+          'Yarinya': 'Girl',
+          'Kudi': 'Money',
+          'Littafi': ' Book',
+          'Makaranta': 'School',
+          'Kaya': 'Clothes',
+          'Duniya': 'World',
+          'Rana': 'Sun',
+          'Dare': 'Night',
+          'Zuciya': 'Heart',
+          'Rafi': 'River',
+          'Uba': 'Father',
+          'Yawa': 'Many',
+          'Lafiya': 'Health',
+          'Rubutu': 'Write',
+          'Hanya': 'Road'}
+
